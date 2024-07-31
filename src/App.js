c98c19fbd28f8a0b97c2b870df75924b70425597
zihua <C109156129@nkust.edu.tw>
2024-07-31 15:55:05 +0800

    “update”

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Player } from "video-react";
 // import Player1 from "./player1";
 // import Player2 from "./player2";
-// import Player3 from "./player3";
-import Player6 from "./player6";
+import Player3 from "./player3";
+// import Player6 from "./player6";
 import './App.css';
 
 function App(){
   
     return (
       <div className="App1">
-        <Player6/>
+        <Player3/>
       </div>
     );
   };
